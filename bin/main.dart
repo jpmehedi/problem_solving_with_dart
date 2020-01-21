604a96fb4377f7b4c6f341dfb862c3089e192a4b
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-21 22:29:33 +0600

    Program to calculate sum of 5 subjects and find percentage

--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,24 @@
-//Program to convert temperature from degree centigrade to Fahrenheit vise a versa
+//Program to calculate sum of 5 subjects and find percentage.
 import 'dart:io';
 import 'dart:math';
-void main (){
 
-  print("Enter value:");
-  var inputValue = stdin.readLineSync();
-  double c = double.parse( inputValue);
+void main(){
+  double math,dataStructure,algorithm,programming,database, per;
+  int  total=500;
 
-  double fahrenheit = c*(9/5)+32;
-  print("$fahrenheit F");
-  double centrigrade = (fahrenheit-32)*5/9;
-  print('$centrigrade C');
+  print("Input Math:");
+  math = double.parse(stdin.readLineSync());
+  print("Input Data Structure:");
+  dataStructure = double.parse(stdin.readLineSync());
+  print("Input Algorithm:");
+  algorithm = double.parse(stdin.readLineSync());
+  print("Input Programming:");
+  programming = double.parse(stdin.readLineSync());
+  print("Input Database:");
+  database = double.parse(stdin.readLineSync());
+  double sumAllSub = math + database + algorithm + programming + database;
+
+  per = (sumAllSub*100)/500;
+  print('Persent mark: $per');
 
 }
