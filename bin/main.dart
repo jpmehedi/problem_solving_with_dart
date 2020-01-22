850a05205787734b0485cb99e8d0df5e8f098a27
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-22 22:58:58 +0600

    Program to find greatest in 3 numbers

--- bin/main.dart
+++ bin/main.dart
@@ -1,17 +1,24 @@
-//Program to show swap of two no’s without using third variable.
+//Program to find greatest in 3 numbers.
 import 'dart:io';
 
 void main(){
 
-  print('Enter value of a:');
-  var a = int.parse(stdin.readLineSync());// 5
-  print('Enter value of b:');
-  var b = int.parse(stdin.readLineSync());// 7
-  a = a + b;// 5+7=12=a
-  b = a - b;// 12-7=5=b
-  a = a - b;// 12-5=7=a
-  print('after Swap  we get value of a:$a');
-  print('after swap we get value of b:$b');
-}
+  print("Enter First Value:");
+  var numberOne = int.parse(stdin.readLineSync());
+  print("Enter Second Value:");
+  var numberTwo = int.parse(stdin.readLineSync());
+  print("Enter Thrid Value:");
+  var numberThree = int.parse(stdin.readLineSync());
 
+  if((numberOne > numberTwo)&&(numberOne >numberThree)){
+    print('Number One is Gratter');
+  }else if((numberTwo > numberOne)&&(numberTwo > numberThree)){
+    print('Number two is Gratter');
+  }else if((numberThree > numberOne)&&(numberThree >numberTwo)){
+    print('Number three is gratter');
+  }
+
+
+
+}
 
